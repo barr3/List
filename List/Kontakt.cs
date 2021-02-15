89d7d81fba447e20f75395da6064dbaae066fb49
barr3 <35341491+barr3@users.noreply.github.com>
2021-02-15 18:44:41 +0100

    Initial commit

---
+++ List/Kontakt.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class Kontakt
+    {
+
+        private string namn;
+        private string telefonNummer;
+        private int födelseÅr;
+
+
+        /*public Kontakt()
+        {
+            
+        }*/
+
+        public void showData()
+        {
+            Console.WriteLine("Namn: " + namn);
+            Console.WriteLine("Telefonnummer: " + telefonNummer);
+            Console.WriteLine("Födelseår: " + födelseÅr);
+            Console.Write("\n\n");
+
+
+        }
+
+        public string getNamn()
+        {
+            return namn;
+        }
+
+        public void setNamn(string namn)
+        {
+            this.namn = namn;
+        }
+
+        public string getNummer()
+        {
+            return telefonNummer;
+        }
+
+        public void setNummer(string telefonNummer)
+        {
+            this.telefonNummer = telefonNummer;
+        }
+
+        public int getFödelseÅr()
+        {
+            return födelseÅr;
+        }
+
+        public void setFödelseÅr(int födelseÅr)
+        {
+            this.födelseÅr = födelseÅr;
+        }
+
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static List<Kontakt> kontakter = new List<Kontakt>();
+
+        static void Main(string[] args)
+        {
+
+
+            inputKontakt();
+
+            showData();
+
+            Console.ReadLine();
+
+        }
+
+        static bool skapaKontakt(int i)
+        {
+            Console.Clear();
+            kontakter.Add(new Kontakt());
+            
+            Console.WriteLine("Vänligen ange namn på kontakten.");
+            kontakter[i].setNamn(Console.ReadLine());
+
+            Console.WriteLine("\nVänligen ange " + kontakter[i].getNamn() + "s telefonnummer.");
+            kontakter[i].setNummer(Console.ReadLine());
+
+            Console.WriteLine("\nVänligen ange " + kontakter[i].getNamn() + "s födelseår.");
+            kontakter[i].setFödelseÅr(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Tryck 1 om du vill lägga till en till kontakt");
+            if (Console.ReadLine() == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        static void inputKontakt()
+        {
+            int i = 0;
+            while (true)
+            {
+                if (skapaKontakt(i) == false)
+                {
+                    break;
+                }
+
+                i++;
+            }
+        }
+
+        static void showData()
+        {
+            Console.Clear();
+            for (int j = 0; j < kontakter.Count; j++)
+            {
+                kontakter[j].showData();
+            }
+
+            Console.WriteLine("Tryck 1 om du vill ändra någon kontakt");
+
+            /*if (Console.ReadLine )
+            {
+
+            }*/
+
+        }
+    }
+}
